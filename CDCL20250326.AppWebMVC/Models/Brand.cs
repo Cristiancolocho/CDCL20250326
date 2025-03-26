87d277576efd72ff5a91cc9ab037e811b622bb6e
Cristian Daniel Colocho López <colochocristian@outlook.com>
2025-03-26 07:49:45 -0600

    migrando  base de datos

---
+++ CDCL20250326.AppWebMVC/Models/Brand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CDCL20250326.AppWebMVC.Models;
+
+public partial class Brand
+{
+    public int Id { get; set; }
+
+    public string BrandName { get; set; } = null!;
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
